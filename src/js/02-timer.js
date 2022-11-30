2018804dc5dd0105adb177343fb4c0ee180a82c4
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2022-11-30 12:46:12 +0200

    task 2-1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,23 @@
-// import 'flatpickr/dist/flatpickr.min.css';
-// import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from 'flatpickr';
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
-// flatpickr('input#datetime-picker', options);
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    refs.startBtn.disabled = false;
+    if (selectedDates[0] <= new Date()) {
+      refs.startBtn.disabled = true;
+      alert('Please choose a date in the future');
+    }
+    console.log(selectedDates[0].getTime());
+  },
+};
+
+flatpickr('input#datetime-picker', options);
