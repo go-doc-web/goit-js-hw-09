1d2c6438d18b960fba8534f639fe5ebf4ce10b49
Oleg Gubskiy <gubskyolegod@gmail.com>
2022-12-03 02:23:37 +0200

    task3-end-1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('form'),
+};
+
+refs.form.addEventListener('submit', onClickSubmit);
+
+function onClickSubmit(e) {
+  e.preventDefault();
+  let delayVal = parseInt(e.currentTarget.delay.value);
+  const stepVal = parseInt(e.currentTarget.step.value);
+  const amountVal = parseInt(e.currentTarget.amount.value);
+  let delay = delayVal;
+  for (let position = 1; position <= amountVal; position += 1) {
+    createPromise({ position, delay })
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += stepVal;
   }
+}
+function createPromise({ position, delay }) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
