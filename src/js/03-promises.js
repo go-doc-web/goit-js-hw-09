319d11e1f596a63869986b0a42a20b71821c9eb7
Oleg <97858843+go-doc-web@users.noreply.github.com>
2022-12-03 02:30:51 +0200

    add comments

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   form: document.querySelector('form'),
